f1b076014488532d6eefd6e9ca4cbd2d14f51f5c
Surekha1919 <surekha.dasari63@gmail.com>
2020-10-03 18:12:13 +0530

    Updates

---
+++ precache-manifest.2d3290e7da47ec103e4e1f0ff6ba07cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774ced5f2f2c6d5e66c56395f76e8787",
+    "url": "/reactlastday/index.html"
+  },
+  {
+    "revision": "76ff5d69e9c1b0c2aefd",
+    "url": "/reactlastday/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "253286a1be32009a2a70",
+    "url": "/reactlastday/static/js/2.7efb534e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactlastday/static/js/2.7efb534e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ff5d69e9c1b0c2aefd",
+    "url": "/reactlastday/static/js/main.8088ded3.chunk.js"
+  },
+  {
+    "revision": "79180f6183161584761c",
+    "url": "/reactlastday/static/js/runtime-main.3e68e482.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactlastday/static/media/logo.5d5d9eef.svg"
+  }
+]);
